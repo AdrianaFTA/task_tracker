cfde36ce9bf75a68595f187cb1010ddf97f254e8
AdrianaFTA <adrianafarmstrong@gmail.com>
2025-12-17 12:15:33 +0000

    fixed test

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,74 +1,51 @@
 class TasksController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_task, only: %i[ show edit update destroy ]
-
-  # GET /tasks or /tasks.json
-  def index
-    @tasks = Task.all
-  end
-
-  # GET /tasks/1 or /tasks/1.json
-  def show
-  end
-
-  # GET /tasks/new
-  def new
-    @task = Task.new
-  end
-
-  # GET /tasks/1/edit
-  def edit
-  end
 
   def index
     @tasks = current_user.tasks.page(params[:page]).per(5)
   end
 
-  # POST /tasks or /tasks.json
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+  end
+
   def create
     @task = current_user.tasks.new(task_params)
 
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to @task, notice: "Task was successfully created." }
-        format.json { render :show, status: :created, location: @task }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    if @task.save
+      redirect_to @task, notice: "Task was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /tasks/1 or /tasks/1.json
   def update
-    respond_to do |format|
-      if @task.update(task_params)
-        format.html { redirect_to @task, notice: "Task was successfully updated.", status: :see_other }
-        format.json { render :show, status: :ok, location: @task }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    if @task.update(task_params)
+      redirect_to @task, notice: "Task was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /tasks/1 or /tasks/1.json
   def destroy
-    @task.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to tasks_path, notice: "Task was successfully destroyed.", status: :see_other }
-      format.json { head :no_content }
-    end
+    @task.destroy
+    redirect_to tasks_path, notice: "Task was successfully destroyed."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_task
-      @task = Task.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def task_params
-      params.expect(task: [ :title, :desription, :due_date, :status ])
-    end
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :due_date, :status)
+  end
 end
--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,8 +1,12 @@
 require "test_helper"
 
 class TasksControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
-    @task = tasks(:one)
+    @user = users(:test_user)
+    sign_in @user
+    @task = tasks(:task_one)
   end
 
   test "should get index" do
@@ -17,7 +21,7 @@ class TasksControllerTest < ActionDispatch::IntegrationTest
 
   test "should create task" do
     assert_difference("Task.count") do
-      post tasks_url, params: { task: { desription: @task.desription, due_date: @task.due_date, status: @task.status, title: @task.title } }
+      post tasks_url, params: { task: { description: @task.description, due_date: @task.due_date, status: @task.status, title: @task.title } }
     end
 
     assert_redirected_to task_url(Task.last)
@@ -34,7 +38,7 @@ class TasksControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update task" do
-    patch task_url(@task), params: { task: { desription: @task.desription, due_date: @task.due_date, status: @task.status, title: @task.title } }
+    patch task_url(@task), params: { task: { description: @task.description, due_date: @task.due_date, status: @task.status, title: @task.title } }
     assert_redirected_to task_url(@task)
   end
 
